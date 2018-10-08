ca46648872d2882841201903cb34ea31f46b1cdb
Tyler Bruno <tybruno@cisco.com>
2018-10-07 19:35:09 -0700

    Created base.html and restructured the front end code

---
+++ detecting_human_emotion_webapp/__init__.py
@@ -0,0 +1,5 @@
+from flask import Flask
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
---
+++ detecting_human_emotion_webapp/app.py
@@ -0,0 +1,28 @@
+from flask import render_template
+from detecting_human_emotion_webapp import app
+import os
+
+
+
+@app.route("/",methods=["GET"])
+def home():
+    header = 'Detecting Human Emotion'
+    data = getLineFromTextFile('detecting_human_emotion_webapp/questions.txt')
+    template_name = "index.html"
+    return render_template(template_name_or_list=template_name,data =data, title = header)
+
+
+def getLineFromTextFile(fileName):
+    print(os.path.exists(fileName))
+    data = ''
+
+    with open(fileName, 'r') as file:
+
+        for line in file:
+            data += line
+
+    return data
+
+
+if __name__ == '__main__':
+    app.run()
