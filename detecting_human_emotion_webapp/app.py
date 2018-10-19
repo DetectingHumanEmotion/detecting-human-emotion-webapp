ca7867f4912f9603457a1f5e656fc5799318e191
Tyler Bruno <tybruno@cisco.com>
2018-10-19 16:37:19 -0700

    Made the code post for each question

--- detecting_human_emotion_webapp/app.py
+++ detecting_human_emotion_webapp/app.py
@@ -1,7 +1,7 @@
-from flask import render_template, redirect
+from flask import render_template, redirect,url_for
 from detecting_human_emotion_webapp import app
 from detecting_human_emotion_webapp.forms import UserInfoForm
-from deception_detection.audio.paura2 import  recordAudioSegments
+# from deception_detection.audio.paura2 import recordAudioSegments
 
 
 
@@ -18,6 +18,21 @@ else:
 
     QUESTIONS = 'questions.txt'
 
+class questions:
+
+    def __init__(self,questions):
+        self.questions = questions
+        self.currentQuestion = 0
+
+    def getSize(self):
+        return len(self.questions)
+
+    def getCurrentQuestion(self):
+        question = self.questions[self.currentQuestion]
+        self.currentQuestion += 1
+
+        return question
+
 
 #This is the python script that should be ran n the html code
 def recordAudioSegments():
@@ -32,7 +47,7 @@ def recordAudioSegments():
     recordAudioSegments(BLOCKSIZE=BLOCKSIZE, model=MODEL, algorithm=ALGORITHM, Fs=FS, showSpectrogram=SHOWSPECTOGRAM,
                         showChromagram=SHOWCHROMOGRAM, recordActivity=RECORDACTIVITY)
 
-
+#
 @app.route("/", methods=["GET"])
 def userInfo():
     header = 'Get User Info'
@@ -52,30 +67,42 @@ def userInfoPost():
 
 
 @app.route("/detecting", methods=["GET"])
-def home():
+def recording_start_get():
+    header = "Start Detecting Human Emotion"
+    template = "startprompt.html"
 
-    header = 'Detecting Human Emotion'
+    prompt = "After clicking on the start button you will be asked a series of question. Once you have read the question please answer to the best of your ability and alaborate on your response."
 
-    data = getListFromTextFile(QUESTIONS)
-    users[-1].questions = data
-    user = users[-1]
+    return render_template(template_name_or_list=template, header = header, prompt=prompt)
+
+@app.route("/detecting",methods=["POST"])
+def recording_start_post():
+    # questions = getListFromTextFile(QUESTIONS)
+    # print(questions)
+    return redirect(url_for('question', questionNumber = 0))
+
+@app.route("/detecting/<int:questionNumber>", methods=["GET"])
+def question(questionNumber):
+    print(type(questionNumber))
+    questions = getListFromTextFile(QUESTIONS)
+    header = f'Question {questionNumber}'
+    template_name = "questions.html"
+
+    q= questions[questionNumber]
 
 
+    return render_template(template_name_or_list=template_name,title=header,question = q)
 
-    print(users[-1].print_data())
+#
 
-    template_name = "index.html"
-    return render_template(template_name_or_list=template_name,user = user, data=data, title=header)
-
-@app.route("/detecting", methods=["POST"])
-def homePost():
-    print("testing that it posted")
-
-@app.route("/dashboard", methods=["GET"])
-def dashBoard():
-    template_name = "dashboard.html"
-    return render_template(template_name_or_list=template_name)
-
+@app.route("/detecting/<int:questionNumber>", methods=["POST"])
+def question_post(questionNumber):
+    questions = getListFromTextFile(QUESTIONS)
+    questionNumber += 1
+    if questionNumber < len(questions):
+        return redirect(url_for('question', questionNumber = questionNumber))
+    else:
+        return redirect("/results")
 
 @app.route("/results",methods=["GET"])
 def resultsPage():
@@ -85,6 +112,28 @@ def resultsPage():
     #passing in as an array
     data = getLineFromTextFile(QUESTIONS).split("\n")
     return render_template(template_name_or_list=template_name, data = data, title = header)
+
+# @app.route("/detecting", methods=["GET"])
+# def home():
+#
+#     header = 'Detecting Human Emotion'
+#
+#     data = getListFromTextFile(QUESTIONS)
+#     users[-1].questions = data
+#     user = users[-1]
+#
+#
+#
+#     print(users[-1].print_data())
+#
+#     template_name = "index.html"
+#     return render_template(template_name_or_list=template_name,user = user, data=data, title=header)
+#
+# @app.route("/detecting", methods=["POST"])
+# def homePost():
+#     print("testing that it posted")
+
+
 
 
 def getLineFromTextFile(fileName):
