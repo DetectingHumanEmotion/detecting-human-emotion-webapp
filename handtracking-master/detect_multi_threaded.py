6762b0ebcd6efc9d0dcd9614244616481af10c89
kuro sora <c.yee1418@gmail.com>
2018-11-07 19:46:48 -0800

    hand and blink

--- handtracking-master/detect_multi_threaded.py
+++ handtracking-master/detect_multi_threaded.py
@@ -6,7 +6,35 @@ from multiprocessing import Queue, Pool
 import time
 from utils.detector_utils import WebcamVideoStream
 import datetime
+
 import argparse
+from scipy.spatial import distance as dist
+from imutils.video import FileVideoStream
+from imutils.video import VideoStream
+from imutils import face_utils
+import numpy as np
+import argparse
+import imutils
+import time
+import dlib
+
+def eye_aspect_ratio(eye):
+	# compute the euclidean distances between the two sets of
+	# vertical eye landmarks (x, y)-coordinates
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+
+	# compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
+	C = dist.euclidean(eye[0], eye[3])
+
+	# compute the eye aspect ratio
+	ear = (A + B) / (2.0 * C)
+
+	# return the eye aspect ratio
+	return ear
+
+
 
 
 frame_processed = 0
@@ -56,7 +84,34 @@ if __name__ == '__main__':
                         default=4, help='Number of workers.')
     parser.add_argument('-q-size', '--queue-size', dest='queue_size', type=int,
                         default=5, help='Size of the queue.')
+    parser.add_argument("-p", "--shape-predictor", required=True,
+                    help="path to facial landmark predictor")
+    parser.add_argument("-v", "--video", type=str, default="",
+                    help="path to input video file")
     args = parser.parse_args()
+
+
+    # define two constants, one for the eye aspect ratio to indicate
+    # blink and then a second constant for the number of consecutive
+    # frames the eye must be below the threshold
+    EYE_AR_THRESH = 0.3
+    EYE_AR_CONSEC_FRAMES = 3
+
+    # initialize the frame counters and the total number of blinks
+    TOTAL_BLINKS = 0
+
+    # initialize dlib's face detector (HOG-based) and then create
+    # the facial landmark predictor
+    print("[INFO] loading facial landmark predictor...")
+    detector = dlib.get_frontal_face_detector()
+    ags=vars(args)
+    predictor = dlib.shape_predictor(ags["shape_predictor"])
+
+    # grab the indexes of the facial landmarks for the left and
+    # right eye, respectively
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
 
     input_q = Queue(maxsize=args.queue_size)
     output_q = Queue(maxsize=args.queue_size)
@@ -83,6 +138,8 @@ if __name__ == '__main__':
     num_frames = 0
     fps = 0
     index = 0
+    BLINK_COUNTER = 0
+    TOTAL_BLINKS = 0
 
     cv2.namedWindow('Multi-Threaded Detection', cv2.WINDOW_NORMAL)
     first = True
@@ -94,6 +151,7 @@ if __name__ == '__main__':
         input_q.put(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         output_frame = output_q.get()
 
+        gray = cv2.cvtColor(output_frame, cv2.COLOR_BGR2GRAY)
         output_frame = cv2.cvtColor(output_frame, cv2.COLOR_RGB2BGR)
 
         # if first == True:
@@ -116,8 +174,62 @@ if __name__ == '__main__':
                     # (img, pts, isClosed, color[, thickness[, lineType[, shift]]]) → None
 
                     number_of_points = len(face_pts)
-                    detector_utils.draw_fps_on_image(
-                        "FPS : " + str(int(fps)), output_frame)
+                    # detector_utils.draw_fps_on_image(
+                    #     "FPS : " + str(int(fps)), output_frame)
+                    # detect faces in the grayscale frame
+                    rects = detector(gray, 0)
+
+                # loop over the face detections
+                for rect in rects:
+                    # determine the facial landmarks for the face region, then
+                    # convert the facial landmark (x, y)-coordinates to a NumPy
+                    # array
+                    # print("rect type: ", type(rect))
+                    # print("output_frame type: ", type(output_frame))
+                    shape = predictor(output_frame, rect)
+                    shape = face_utils.shape_to_np(shape)
+
+                    # extract the left and right eye coordinates, then use the
+                    # coordinates to compute the eye aspect ratio for both eyes
+                    leftEye = shape[lStart:lEnd]
+                    rightEye = shape[rStart:rEnd]
+                    leftEAR = eye_aspect_ratio(leftEye)
+                    rightEAR = eye_aspect_ratio(rightEye)
+
+                    # average the eye aspect ratio together for both eyes
+                    ear = (leftEAR + rightEAR) / 2.0
+
+                    # compute the convex hull for the left and right eye, then
+                    # visualize each of the eyes
+                    leftEyeHull = cv2.convexHull(leftEye)
+                    rightEyeHull = cv2.convexHull(rightEye)
+                    cv2.drawContours(output_frame, [leftEyeHull], -1, (0, 255, 0), 1)
+                    cv2.drawContours(output_frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+                    # check to see if the eye aspect ratio is below the blink
+                    # threshold, and if so, increment the blink frame counter
+                    if ear < EYE_AR_THRESH:
+                        BLINK_COUNTER += 1
+
+                    # otherwise, the eye aspect ratio is not below the blink
+                    # threshold
+                    else:
+                        # if the eyes were closed for a sufficient number of
+                        # then increment the total number of blinks
+                        if BLINK_COUNTER >= EYE_AR_CONSEC_FRAMES:
+                            TOTAL_BLINKS += 1
+
+                        # reset the eye frame counter
+                        BLINK_COUNTER = 0
+
+                    # draw the total number of blinks on the frame along with
+                    # the computed eye aspect ratio for the frame
+                    cv2.putText(output_frame, "Blinks: {}".format(TOTAL_BLINKS), (10, 30),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                    cv2.putText(output_frame, "EAR: {:.2f}".format(ear), (200, 30),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+
                 cv2.imshow('Multi-Threaded Detection', output_frame)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
@@ -126,8 +238,12 @@ if __name__ == '__main__':
                     num_frames = 0
                     start_time = datetime.datetime.now()
                 else:
-                    print("frames processed: ",  index,
-                          "elapsed time: ", elapsed_time, "fps: ", str(int(fps)))
+                    # print("frames processed: ",  index,
+                    #       "elapsed time: ", elapsed_time, "fps: ", str(int(fps)))
+                    cv2.putText(frame, "Blinks: {}".format(TOTAL_BLINKS), (10, 30),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                    cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
         else:
             # print("video end")
             break
