5cdb41c59d6511ac6accaa493ccc23381fc713a3
CindyYee <c.yee1418@gmail.com>
2018-12-06 13:18:52 -0800

    fixed blink EAR threshold

--- deception_detection/visual/detect_multi_threaded.py
+++ deception_detection/visual/detect_multi_threaded.py
@@ -147,7 +147,7 @@ def record():
     # define two constants, one for the eye aspect ratio to indicate
     # blink and then a second constant for the number of consecutive
     # frames the eye must be below the threshold
-    EYE_AR_THRESH = 0.3
+    EYE_AR_THRESH = 0.2
     EYE_AR_CONSEC_FRAMES = 3
 
     # initialize the frame counters and the total number of blinks
